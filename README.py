eaa74cd77ecdef44b4f56ef488cce63765cc88d5
Bonnie Magland <bonnie.magland@gmail.com>
2020-08-20 20:59:17 -0400

    Bonnie's suggested additions

--- README.py
+++ README.py
@@ -1,9 +1,11 @@
 print("hey everyone! how's your day going?")
 print("psst... say good or bad")
 command=input('how was your day?')
-if command = 'good'
+if command == 'good':
     print("should have said bad!")
     print("proceeds to murder with rubber chicken")
+    print('ERROR: You are ded.')
 else:
     print("should have said good!")
     print("proceeds to murder with deli meat")
+    print('ERROR: You are ded.')
