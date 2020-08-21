6c2bdd3b1cd55ebdbb83b21b80ef5a7aa3fd195e
william magland <williammagland@pop-os.localdomain>
2020-08-20 20:39:43 -0400

    README.py

---
+++ README.py
@@ -0,0 +1,9 @@
+print("hey everyone! how's your day going?")
+print("psst... say good or bad")
+command=input('how was your day?')
+if command = 'good'
+    print("should have said bad!")
+    print("proceeds to murder with rubber chicken")
+else:
+    print("should have said good!")
+    print("proceeds to murder with deli meat")
